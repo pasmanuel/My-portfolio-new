2645a250dea952f5d03ba6357fe85ac93aae595b
Oppong Emmanuel <pasmanuel96@gmail.com>
2024-02-21 20:08:22 +0000

    nav bar fixed

---
+++ index.js
@@ -0,0 +1,14 @@
+let openHam = document.querySelector('#openHam');
+let closeHam = document.querySelector('#closeHam');
+let navigationItems = document.querySelector('#navigationItems');
+
+const hamburgerEvent = (navigation, close, open) => {
+    navigationItems.style.display = navigation;
+    closeHam.style.display = close;
+    openHam.style.display = open;
+
+   
+};
+
+openHam.addEventListener('click', () => hamburgerEvent("flex", "block", "none"));
+closeHam.addEventListener('click', () => hamburgerEvent("none", "none", "block"));
